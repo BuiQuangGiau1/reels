66a545f92d45764dbfe500aa5a4514247ebf31d1
BuiQuangGiau1 <114122977+BuiQuangGiau1@users.noreply.github.com>
2023-12-06 20:23:56 +0700

    Delete src directory

--- src/Axios.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-  baseURL: "your-backend-heroku-url"  //thay thế "your-backend-heroku-url" bằng URL của backend s
-});
-
-export default axiosInstance;
